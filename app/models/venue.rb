56a08af43b18d5966262829e941a5ac70f03e3c5
Arik Grahl <mail@arik-grahl.de>
2016-08-18 18:24:52 +0200

    implement presence validation for venues

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,4 +1,7 @@
 class Venue < ApplicationRecord
   has_many :events
   belongs_to :location, dependent: :destroy
+
+  validates :name, :street, :zip_code, :city,
+            presence: true
 end
--- spec/models/venue_spec.rb
+++ spec/models/venue_spec.rb
@@ -4,4 +4,12 @@ RSpec.describe Venue, type: :model do
   it { should have_many(:events) }
 
   it { should belong_to(:location).dependent(:destroy) }
+
+  it { should validate_presence_of(:name) }
+
+  it { should validate_presence_of(:street) }
+
+  it { should validate_presence_of(:zip_code) }
+
+  it { should validate_presence_of(:city) }
 end
