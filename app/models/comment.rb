deaeb9e07555b0041a06217b4111f7001fe4b5e1
Arik Grahl <mail@arik-grahl.de>
2016-08-18 17:30:57 +0200

    implement presence validation for comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :event
+
+  validates :event, :user, :message,
+            presence: true
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -4,4 +4,10 @@ RSpec.describe Comment, type: :model do
   it { should belong_to(:user) }
 
   it { should belong_to(:event) }
+
+  it { should validate_presence_of(:message) }
+
+  it { should validate_presence_of(:user) }
+
+  it { should validate_presence_of(:event) }
 end
