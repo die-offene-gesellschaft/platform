d9f2f3ec839b2fcb201c846e6fe606a93901f6de
Arik Grahl <mail@arik-grahl.de>
2016-08-18 17:37:07 +0200

    implement presence validation for events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,9 +1,14 @@
 class Event < ApplicationRecord
+  belongs_to :user
+  belongs_to :venue
+
+  has_many :comments, dependent: :destroy
+  has_many :pictures, dependent: :destroy
+
   has_and_belongs_to_many :participants,
                           class_name: 'User',
                           join_table: 'event_participations'
-  belongs_to :user
-  has_many :comments, dependent: :destroy
-  has_many :pictures, dependent: :destroy
-  belongs_to :venue
+
+  validates :title, :event_type, :begin_at, :end_at, :description, :venue,
+            presence: true
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -14,4 +14,16 @@ RSpec.describe Event, type: :model do
       .class_name('User')
       .join_table('event_participations')
   end
+
+  it { should validate_presence_of(:title) }
+
+  it { should validate_presence_of(:event_type) }
+
+  it { should validate_presence_of(:begin_at) }
+
+  it { should validate_presence_of(:end_at) }
+
+  it { should validate_presence_of(:description) }
+
+  it { should validate_presence_of(:venue) }
 end
