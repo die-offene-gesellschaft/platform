5e4d2aed133b64927de2c3233232973c264ae226
Arik Grahl <mail@arik-grahl.de>
2016-09-26 20:26:27 +0200

    implement newsletter integration

--- app/models/newsletter.rb
+++ app/models/newsletter.rb
@@ -1,4 +1,26 @@
 class Newsletter < ApplicationRecord
+  after_commit :sync_to_mailchimp_later,
+               on: :create
+
   validates :forename, :surname, :email,
             presence: true
+
+  def sync_to_mailchimp_later
+    delay.sync_to_mailchimp
+  end
+
+  private
+
+  def sync_to_mailchimp
+    gb = Gibbon::API.new
+    gb.lists.subscribe id: ENV['MAILCHIMP_LIST_ID'],
+                       email: {
+                         email: email
+                       },
+                       merge_vars: {
+                         FNAME: forename,
+                         LNAME: surname
+                       },
+                       double_optin: false
+  end
 end
