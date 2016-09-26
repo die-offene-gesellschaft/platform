ff5cccd82353292ab7723799db145926d450b244
Arik Grahl <mail@arik-grahl.de>
2016-09-26 13:37:27 +0200

    satisfy linting and tests

--- spec/models/newsletter_spec.rb
+++ spec/models/newsletter_spec.rb
@@ -1,7 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Newsletter, type: :model do
-  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:forename) }
+
+  it { should validate_presence_of(:surname) }
 
   it { should validate_presence_of(:email) }
 end
