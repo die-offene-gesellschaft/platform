e30bee2d7d728dc82e9c8f14ac887411166384e9
Arik Grahl <mail@arik-grahl.de>
2016-09-26 15:08:13 +0200

    configure smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,13 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: ENV['DOMAIN'] }
 
+  config.action_mailer.smtp_settings = {
+                                         address: ENV['SMTP_ADDRESS'],
+                                         domain: ENV['SMTP_DOMAIN'],
+                                         user_name: ENV['SMTP_USERNAME'],
+                                         password: ENV['SMTP_PASSWORD']
+                                       }
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
