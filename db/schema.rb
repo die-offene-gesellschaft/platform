f0e4a1ebd769665103a9864363f2d5cac0d8486f
Arik Grahl <mail@arik-grahl.de>
2016-09-27 12:38:34 +0200

    add newsletter and terms_of_use flags to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160926093456) do
+ActiveRecord::Schema.define(version: 20160927103218) do
 
   create_table "active_members", force: :cascade do |t|
     t.string   "forename"
@@ -220,6 +220,8 @@ ActiveRecord::Schema.define(version: 20160926093456) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string   "unconfirmed_email"
+    t.boolean  "newsletter"
+    t.boolean  "terms_of_use"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
