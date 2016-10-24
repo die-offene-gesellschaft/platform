4dec8f6fcd2e16151882a58e9556c4388f77cb44
Arik Grahl <mail@arik-grahl.de>
2016-10-24 18:14:41 +0200

    implement improved home site with user shuffle concept

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,14 @@
 class HomeController < ApplicationController
   before_action :set_events, only: [:show]
-  before_action :set_users, only: [:show]
   before_action :set_video_users, only: [:show]
+  before_action :set_frontpage_users, only: [:show]
+  before_action :set_vip_users, only: [:show]
+  before_action :set_users, only: [:show]
+
+  USERS_COUNT = 10
+  FRONTPAGE_USERS_COUNT = 4
+  VIDEO_USERS_COUNT = 2
+  VIP_USERS_COUNT = 8
 
   def show
     @contents = [
@@ -20,17 +27,33 @@ class HomeController < ApplicationController
 
   def set_users
     @users = User.where(locked: false)
-                 .where(video_url: [nil, ''])
                  .where.not(avatar_file_name: nil)
-                 .limit(25)
-                 .shuffle
+                 .where.not(
+                   id: @video_users.map(&:id) +
+                       @frontpage_users.map(&:id) +
+                       @vip_users.map(&:id)
+                 )
+                 .order(created_at: :desc)
+                 .limit(USERS_COUNT)
   end
 
   def set_video_users
     @video_users = User.where(locked: false)
                        .where.not(video_url: [nil, ''])
                        .where.not(avatar_file_name: nil)
-                       .limit(2)
-                       .shuffle
+                       .sample(VIDEO_USERS_COUNT)
+  end
+
+  def set_frontpage_users
+    @frontpage_users = User.where(locked: false)
+                           .where(frontpage: true)
+                           .where.not(avatar_file_name: nil)
+                           .sample(FRONTPAGE_USERS_COUNT)
+  end
+
+  def set_vip_users
+    @vip_users = User.where(locked: false)
+                     .where(vip: true)
+                     .where.not(avatar_file_name: nil)
   end
 end
