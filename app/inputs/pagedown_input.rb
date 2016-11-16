968cc2fbcfb53a9d6f936fa7058fd154512b637f
Michael Schmidt <nomad5000@yahoo.com>
2016-11-16 11:37:30 +0100

    a few fixes

--- app/inputs/pagedown_input.rb
+++ app/inputs/pagedown_input.rb
@@ -3,7 +3,7 @@ class PagedownInput < SimpleForm::Inputs::TextInput
     out = "<div id=\"wmd-button-bar-#{attribute_name}\"></div>#{wmd_input}"
 
     if input_html_options[:preview]
-      out << "<div id=\"wmd-preview-#{attribute_name}\" class=\"wmd-preview\"></div>"
+      out << "<div class='wmd-preview-title'>Preview</div><div id=\"wmd-preview-#{attribute_name}\" class=\"wmd-preview\"></div>"
     end
 
     out.html_safe
@@ -12,10 +12,12 @@ class PagedownInput < SimpleForm::Inputs::TextInput
   private
 
   def wmd_input
+    classes = input_html_options[:class] || []
+    classes << 'wmd-input form-control'
     @builder.text_area(
       attribute_name,
       input_html_options.merge(
-        class: 'wmd-input form-control', id: "wmd-input-#{attribute_name}"
+        class: classes, id: "wmd-input-#{attribute_name}"
       )
     )
   end
