61c52002adb1c678d4e4e753baa5cd4f02702b42
Michael Schmidt <nomad5000@yahoo.com>
2016-11-28 13:55:48 +0100

    added order to slides

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161123112104) do
+ActiveRecord::Schema.define(version: 20161128122944) do
 
   create_table "active_members", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "forename"
@@ -244,6 +244,7 @@ ActiveRecord::Schema.define(version: 20161123112104) do
     t.datetime "created_at",                         null: false
     t.datetime "updated_at",                         null: false
     t.string   "slide_mode",                         null: false
+    t.integer  "order"
   end
 
   create_table "statements", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
